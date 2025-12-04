a57c1465c2ca4a8e697c20c055758bedc3708994
Salauddin Ansari <salauddinansari07457@gmail.com>
2025-12-04 13:19:40 +0530

    student class

---
+++ src/DeepAndShallow/Student.java
@@ -0,0 +1,14 @@
+package DeepAndShallow;
+
+public class Student {
+
+    public String studentRollNo;
+    public Student(String studentRollNo){
+
+        this.studentRollNo=studentRollNo;
+    }
+
+    public void setStudentRollNo(String studentRollNo) {
+        this.studentRollNo = studentRollNo;
+    }
+}
